58078fd8d08ee5dc9032e00a0fa342806318e481
Bruno Lloret <caballoret@gmail.com>
2024-11-16 09:58:11 -0800

    new interfaces

--- shared/shared.ts
+++ shared/shared.ts
@@ -1,21 +1,86 @@
 
 export namespace FormComponents {
 
-    interface Checkbox {
-        checked: boolean
+    interface BaseQuestion {
+        id: string;
+        label: string;
+        required: boolean;
+        type: QuestionType;
     }
 
-    interface RadioButton {
-        checked: boolean[]
+    type QuestionType = 'checkbox' | 'radio' | 'dropdown' | 'open' | 'matrix';
+
+    interface Checkbox extends BaseQuestion {
+        type: 'checkbox';
+        checked: boolean;
+        label: string;
     }
 
-    interface Dropdown {
-        options: string[]
+    interface RadioButton extends BaseQuestion {
+        type: 'radio';
+        options: RadioOption[];
+        selectedOptionId: string | null;
+    }
+
+    interface RadioOption {
+        id: string;
+        label: string;
+    }
+
+
+    interface Dropdown extends BaseQuestion {
+        type: 'dropdown';
+        options: DropdownOption[];
+        selectedOptionId: string | null;
+    }
+
+    interface DropdownOption {
+        id: string;
+        label: string;
+        value: string;
+    }
+
+
+    interface MatrixQuestion {
+        type: 'matrix';
+        options: MatrixOption[];
+        selectedOptionId: number | null;
+    }
+
+    interface MatrixOption {
+        id: number;
+        label: string;
+        value: string;
+    }
+
+    interface OpenQuestion extends BaseQuestion {
+        type: 'open';
+        question: string;
+        answer: string | null;
+        placeholder?: string;
+        maxLength?: number;
     }
 
     interface Survey {
-        title: string,
-        question: string | Checkbox | RadioButton | Dropdown,
+        id: string;
+        title: string;
+        questions: (OpenQuestion | Checkbox | RadioButton | Dropdown)[];
+        isPublished: boolean;
+    }
+
+    //Response types
+
+    interface SurveyResponse {
+        id: string;
+        surveyId: string;
+        respondentId?: string;
+        submitteddAt: Date;
+        answers: QuestionResponse[];
+    }
+
+    type QuestionResponse = {
+        questionId: string;
+        value: string | boolean | string[] | null;
     }
 
 }
